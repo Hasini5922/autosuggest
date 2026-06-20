661d76623cb32f7214005de19bbc77ccd8ea9f3a
Hasini <dudikatlahasini@gmail.com>
2026-06-20 10:01:33 +0530

    created toggle

---
+++ frontend/js/toggle.js
@@ -0,0 +1,22 @@
+var users = [
+    {
+        "name": "John Doe",
+        "gender": "Male",
+        "image": "/images/john.png"
+    },
+    {
+        "name": "Jane Doe",
+        "gender": "Female",
+        "image": "/images/jane.png"
+    }
+]
+
+var curId=0;
+function toggle(){
+    curId = (curId+1) % 2;
+    var user=users[curId];
+    document.getElementById("user-image").src=user.image;
+    document.getElementById("user-name").innerText=user.name;
+    document.getElementById("user-gender").innerText=user.gender;
+
+}
